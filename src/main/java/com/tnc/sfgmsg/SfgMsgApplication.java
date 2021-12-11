96f6d00dfc80a26150401788d5ee5258c94f7c64
TncBogdan <tncbogdan@gmail.com>
2021-12-11 12:26:58 +0200

    Initial commit

---
+++ src/main/java/com/tnc/sfgmsg/SfgMsgApplication.java
@@ -0,0 +1,13 @@
+package com.tnc.sfgmsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgMsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tnc/sfgmsg/SfgMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnc.sfgmsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
